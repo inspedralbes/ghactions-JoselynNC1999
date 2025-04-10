10fcb4e61c561302997dc47b22dde31321ac6cbf
a23josnincal <a23josnincal@inspedralbes.cat>
2025-04-10 21:57:36 +0200

    Afegida app Vue amb calculadora

---
+++ calculadoraJoselyn/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Teclat from './components/Teclat.vue'
+</script>
+
+<template>
+  <h1>Calculadora Vue</h1>
+  <Teclat />
+</template>
---
+++ calculadoraJoselyn/src/components/Teclat.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="teclat">
+      <button v-for="n in 9" :key="n" @click="clicat(n)">
+        {{ n }}
+      </button>
+      <p v-if="numero !== null">Has clicat el número: {{ numero }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  const numero = ref(null)
+  const clicat = (n) => {
+    numero.value = n
+  }
+  </script>
+  
+  <style>
+  .teclat {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+  .teclat button {
+    width: 50px;
+    height: 50px;
+    font-size: 20px;
+  }
+  </style>
+  
